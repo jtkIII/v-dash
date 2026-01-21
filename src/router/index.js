c876c781f766d36c61aabb6d5dd82262552899f8
jtkIII <me@jtk3.com>
2026-01-20 18:39:45 -0600

    hash mode 4 github

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'home', component: HomeView, meta: { title: 'Vue Dashboard' } },
     { path: '/about', name: 'about', component: AboutView, meta: { title: 'About' } },
